15317bf9861f4c4e10e899bbbfc820b5b8584fea
Anton <anton.sipin@gmail.com>
2024-01-29 11:07:52 +0300

    Minor fixes

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
+import { TypegooseModule } from 'nestjs-typegoose';
+import { AuthModel } from './auth.model/auth.model'
 
 @Module({
   controllers: [AuthController],
+  imports: [
+    TypegooseModule.forFeature([
+      {
+        typegooseClass: AuthModel
+      }
+    ])
+  ]
 })
 export class AuthModule {}
