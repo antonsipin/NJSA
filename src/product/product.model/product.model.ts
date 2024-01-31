dc82d6af9fdc52dac46f270640cffb215204d3bf
Anton <anton.sipin@gmail.com>
2024-01-31 23:55:37 +0300

    Minor fixes

--- src/product/product.model/product.model.ts
+++ src/product/product.model/product.model.ts
@@ -1,17 +1,45 @@
-export class ProductModel {
-  _id: string;
+import { prop } from '@typegoose/typegoose';
+import { Base, TimeStamps } from '@typegoose/typegoose/lib/defaultClasses';
+
+class ProductCharacteristic {
+  name: string;
+  value: string;
+}
+
+export interface ProductModel extends Base {}
+export class ProductModel extends TimeStamps {
+  @prop()
   image: string;
+
+  @prop()
   title: string;
+
+  @prop()
   price: number;
+
+  @prop()
   oldPrice: number;
+
+  @prop()
   credit: number;
+  @prop()
   calculatedRating: number;
+
+  @prop()
   description: string;
+
+  @prop()
   advanteges: string;
+
+  @prop()
   disAdvantages: string;
+
+  @prop({ type: () => [String] })
   categories: string[];
-  tags: string;
-  characteristics: {
-    [key: string]: string;
-  };
+
+  @prop({ type: () => [String] })
+  tags: string[];
+
+  @prop({ type: () => [ProductCharacteristic] })
+  characteristics: ProductCharacteristic[];
 }
