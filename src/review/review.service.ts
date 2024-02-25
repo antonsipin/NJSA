d6d8c5da866cea4a154b157758eaa1c435222bcf
Anton <anton.sipin@gmail.com>
2024-02-25 18:08:09 +0300

    Fixed review service

--- src/review/review.service.ts
+++ src/review/review.service.ts
@@ -1,10 +1,13 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { ReviewModel } from './review.model/review.model';
-import { ModelType } from '@typegoose/typegoose/lib/types';
+import { ModelType, DocumentType } from '@typegoose/typegoose/lib/types';
+import { CreateReviewDto } from './dto/create-review.dto';
 
 @Injectable()
 export class ReviewService {
 	constructor(@Inject(ReviewModel) private readonly reviewModel: ModelType<ReviewModel>) {}
 
-	
+	async create(dto: CreateReviewDto): Promise<DocumentType<ReviewModel>> {
+		return this.reviewModel.create(dto);
+	}
 }
